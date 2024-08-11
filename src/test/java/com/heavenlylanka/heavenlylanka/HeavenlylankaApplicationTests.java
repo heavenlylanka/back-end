7693f71156fafab84e4eb8ccb63fb1d49ac2bb4e
W.M.U.N. Bandara <udithanuwanbandara2@gmail.com>
2024-08-11 18:40:55 +0000

    update

---
+++ src/test/java/com/heavenlylanka/heavenlylanka/HeavenlylankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heavenlylanka.heavenlylanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavenlylankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
